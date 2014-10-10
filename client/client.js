e1f3cbb4834fbfcd47257ac91aa538451fcdc69a
Nikhil Khanna <nikhilkh@gmail.com>
2014-10-10 12:19:24 -0700

    add iron router and routing

--- client/client.js
+++ client/client.js
@@ -1,37 +1,48 @@
-  // counter starts at 0
-  Meteor.subscribe("codingSessions");
-  Template.hello.helpers({
 
-    codeArea: function() {
-      if(codingSessions.find().fetch()[0] == null)
-        return "0";
-      return codingSessions.find().fetch()[0].codeString;
+//router templates
+Router.map(function(){
+    this.route('intro');
+});
+
+Router.map(function() {
+  this.route('hello', {path: '/'});
+});
+
+
+// counter starts at 0
+Meteor.subscribe("codingSessions");
+Template.hello.helpers({
+
+  codeArea: function() {
+    if(codingSessions.find().fetch()[0] == null)
+      return "0";
+    return codingSessions.find().fetch()[0].codeString;
+  }
+
+});
+
+Template.hello.events({
+  'input textarea': function () {
+    
+    var currentSession = codingSessions.findOne({user: "niki"});
+
+    if(currentSession == null) {
+      return;
     }
 
-  });
-
-  Template.hello.events({
-    'input textarea': function () {
-      
-      var currentSession = codingSessions.findOne({user: "niki"});
-
-      if(currentSession == null) {
-        return;
+    codingSessions.update(
+      {_id: currentSession._id},
+      { 
+        user: "niki",
+        codeString: $('#codeArea').val()
       }
+    );
 
-      codingSessions.update(
-        {_id: currentSession._id},
-        { 
-          user: "niki",
-          codeString: $('#codeArea').val()
-        }
-      );
+  },
 
-    },
+  'click button': function() {
+    var jsString = $('#codeArea').val();
+    eval(jsString);
+  }
 
-    'click button': function() {
-      var jsString = $('#codeArea').val();
-      eval(jsString);
-    }
-
-  });
+});
--- server/server.js
+++ server/server.js
@@ -1,8 +1,7 @@
-
-  Meteor.startup(function () {
-    //clearing the array
-    codingSessions.remove({});
-    //adding a new count
-    codingSessions.insert({user: "niki", codeString: ""});
-    // code to run on server at startup
-  });
+Meteor.startup(function () {
+	//clearing the array
+	codingSessions.remove({});
+	//adding a new count
+	codingSessions.insert({user: "niki", codeString: ""});
+	// code to run on server at startup
+});
